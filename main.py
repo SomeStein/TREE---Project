c12c4d0fc87a04dcfc2081fe2fabf85e52f04285
Aaron Pumm <pummaaron@gmail.com>
2024-03-05 03:26:21 +0100

    new file:   agent_algos.py 	new file:   helping_functions.py 	modified:   main.py 	new file:   simulation.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-import numpy as np
+from simulation import *
+import cProfile
 
-if __name__ == "__main__":
-    print("Hello World!")
-    arr = np.array([1,2,3])
-    print(arr)
-    
+
+width = 60000
+height = 60000
+size = (height, width)
+i = 100000
+s = 30
+n = 1000
+
+# Create a cProfile object
+profiler = cProfile.Profile()
+
+# Run the simulation method with profiling
+profiler.enable()
+
+init = [(2, 2)]*n
+print("\ni: ", i, " s: ", s, " n: ", n)
+framework = Framework(size, init, s, i)
+sparse_matrices_list, took1 = framework.simulate(size_inclusion_random_walk_np)
+
+profiler.disable()
+
+# Print the profiling results
+profiler.print_stats(sort='tottime')
+
+for sparse_matrix in sparse_matrices_list:
+    print(sparse_matrix.toarray(), "\n")
