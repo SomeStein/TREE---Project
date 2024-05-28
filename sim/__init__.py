459e2e5014456c60576af972bfc9f54df1bec7be
Aaron Pumm <pummaaron@gmail.com>
2024-05-28 05:16:01 +0200

    modified:   sim/__init__.py

--- sim/__init__.py
+++ sim/__init__.py
@@ -2,9 +2,9 @@
 # app usage:
 # give rules for SF and DF
 # give agent update rules
-# from all that create Board instance
-# use board instance to calculate info of interest (concurrent with mp)
+# from all that create Framework instance
+# use Framework instance to calculate info of interest (concurrent with mp)
 # (monte carlo density over time, monte carlo average time that amount x gets through doors, SF, DF)
 # Board instance creates timeline data
 # use timeline data to visualize through renderer
-# compare timeline data of different board instances
+# compare timeline data of different Framework instances
